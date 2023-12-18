b2ff8ad33a26d7f3de5667938fc9d3f75b619ca8
MaxOvs19 <gshk3669@gmail.com>
2023-12-18 18:07:26 +0300

    Added header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,34 @@
-import styles from "./Header.module.scss";
+import Image from "next/image";
+import "./header.scss";
 
 const Header = () => {
-  return <header className={styles.header}>header</header>;
+  return (
+    <header className="header">
+      <Image
+        src="/logo.png"
+        width={200}
+        height={64}
+        alt="@"
+        className="header__logo"
+      />
+
+      <nav className="header__nav">
+        <ul>
+          <li>Home</li>
+          <li>About us</li>
+          <li>Support</li>
+        </ul>
+      </nav>
+
+      <Image
+        src="/userIcon.png"
+        width={30}
+        height={30}
+        alt="@"
+        className="header__user"
+      />
+    </header>
+  );
 };
 
 export default Header;
